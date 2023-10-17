d5a863e23a5d5e852fb78adfd313f6787dc86350
Ivan Matcuka <ivanmatcuka@protonmail.com>
2023-10-17 21:55:34 +0200

    WIP: buttons

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -6,26 +6,36 @@ import theme from '../theme';
 type ButtonVariant = 'default' | 'red' | 'outline';
 type ButtonCSSProperties = Pick<
   CSSProperties,
-  'color' | 'backgroundColor' | 'cursor'
+  'color' | 'backgroundColor' | 'cursor' | 'borderColor'
 >;
 type ButtonStyles = Record<ButtonVariant, ButtonCSSProperties>;
 
 const buttonStyles: ButtonStyles = {
   default: {
     backgroundColor: theme.palette.yellow,
+    borderColor: theme.palette.black,
     color: theme.palette.black,
   },
   red: {
     backgroundColor: theme.palette.red,
+    borderColor: theme.palette.white,
     color: theme.palette.white,
   },
   outline: {
     backgroundColor: 'transparent',
+    borderColor: theme.palette.black,
     color: theme.palette.black,
   },
 };
 
-const StyledButton = styled(MUIButton)({});
+const StyledButton = styled(MUIButton)({
+  padding: '10px 20px',
+
+  border: 'solid 2px',
+  borderRadius: 24,
+
+  height: 48,
+});
 
 interface ButtonProps {
   variant: ButtonVariant;
