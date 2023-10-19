8c0e27dd9f8cf8297a77c42feac9154fc4db5092
Ivan Matcuka <ivanmatcuka@protonmail.com>
2023-10-19 21:49:59 +0200

    chore: linting fixed

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,46 +1,46 @@
-import React, { CSSProperties, FC, PropsWithChildren } from 'react';
-import { Button as MUIButton, styled } from '@mui/material';
+// import React, { CSSProperties, FC, PropsWithChildren } from 'react';
+// import { Button as MUIButton, styled } from '@mui/material';
 
-import theme from '../theme';
+// import theme from '../theme';
 
-type ButtonVariant = 'default' | 'red' | 'outline';
-type ButtonCSSProperties = Pick<
-  CSSProperties,
-  'color' | 'backgroundColor' | 'cursor'
->;
-type ButtonStyles = Record<ButtonVariant, ButtonCSSProperties>;
+// type ButtonVariant = 'default' | 'red' | 'outline';
+// type ButtonCSSProperties = Pick<
+//   CSSProperties,
+//   'color' | 'backgroundColor' | 'cursor'
+// >;
+// type ButtonStyles = Record<ButtonVariant, ButtonCSSProperties>;
 
-const buttonStyles: ButtonStyles = {
-  default: {
-    backgroundColor: theme.palette.yellow,
-    color: theme.palette.black,
-  },
-  red: {
-    backgroundColor: theme.palette.red,
-    color: theme.palette.white,
-  },
-  outline: {
-    backgroundColor: 'transparent',
-    color: theme.palette.black,
-  },
-};
+// const buttonStyles: ButtonStyles = {
+//   default: {
+//     backgroundColor: theme.palette.yellow,
+//     color: theme.palette.black,
+//   },
+//   red: {
+//     backgroundColor: theme.palette.red,
+//     color: theme.palette.white,
+//   },
+//   outline: {
+//     backgroundColor: 'transparent',
+//     color: theme.palette.black,
+//   },
+// };
 
-const StyledButton = styled(MUIButton)({});
+// const StyledButton = styled(MUIButton)({});
 
-interface ButtonProps {
-  variant: ButtonVariant;
-  state?: 'disabled';
-}
-export const Button: FC<PropsWithChildren<ButtonProps>> = ({
-  children,
-  variant,
-  state,
-}) => {
-  const style = buttonStyles[variant];
+// interface ButtonProps {
+//   variant: ButtonVariant;
+//   state?: 'disabled';
+// }
+// export const Button: FC<PropsWithChildren<ButtonProps>> = ({
+//   children,
+//   variant,
+//   state,
+// }) => {
+//   const style = buttonStyles[variant];
 
-  if (state === 'disabled') {
-    style.cursor = 'not-allowed';
-  }
+//   if (state === 'disabled') {
+//     style.cursor = 'not-allowed';
+//   }
 
-  return <StyledButton style={style}>{children}</StyledButton>;
-};
+//   return <StyledButton style={style}>{children}</StyledButton>;
+// };
