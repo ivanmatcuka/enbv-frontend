23444417b854ebeeddaa04e58daf464aeb5cce8a
Ivan Matcuka <ivanmatcuka@protonmail.com>
2023-11-07 20:46:17 +0100

    feat: button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,76 +1,81 @@
+'use client';
+
 import { Button as MUIButton, styled } from '@mui/material';
 import { FC, PropsWithChildren } from 'react';
 
+import { PaletteColors } from '../../theming/theme/palette';
 import { Typography } from '../Typography/Typography';
 
-const StyledMUIButton = styled(MUIButton)(({ theme }) => ({
+const Container = styled('div')(() => ({
   position: 'relative',
 
-  height: 36,
-  border: `solid 2px ${theme.palette.brand.black}`,
-  borderRadius: 18,
-  padding: theme.spacing(0, 3),
+  '&:hover': {
+    '.button__body': {
+      transform: 'rotate(2.86deg)',
+    },
+    '.button__background': {
+      transform: 'rotate(-2.92deg)',
+    },
+  },
+}));
 
-  '&:before': {
-    content: '""',
+const Background = styled('div')<{ variant?: 'red' | 'default' }>(({
+  variant = 'default',
+  theme,
+}) => {
+  const palette: PaletteColors[] =
+    variant === 'default'
+      ? ['yellow', 'yellow20', 'yellow40']
+      : ['red', 'red20', 'red40'];
 
+  return {
     position: 'absolute',
-    right: 0,
-    bottom: -10,
-    left: 0,
-    zIndex: -1,
+    inset: '-8px 0 0 0',
+    zIndex: 100,
 
     transform: 'rotate(1.8deg)',
 
     height: 54,
     borderRadius: '50%',
 
-    backgroundColor: theme.palette.brand.yellow,
-  },
+    backgroundColor: palette ? theme.palette.brand[palette[0]] : undefined,
 
-  '&:hover': {
-    '&:before': {
-      backgroundColor: theme.palette.brand.yellow20,
+    '&:hover': {
+      backgroundColor: theme.palette.brand[palette[1]],
     },
-  },
 
-  '&:active': {
-    '&:before': {
-      backgroundColor: theme.palette.brand.yellow40,
+    '&:active': {
+      backgroundColor: theme.palette.brand[palette[2]],
     },
-  },
+  };
+});
+
+const StyledMUIButton = styled(MUIButton)(({ theme }) => ({
+  position: 'relative',
+  zIndex: 200,
+
+  height: 36,
+  border: `solid 2px ${theme.palette.brand.black}`,
+  borderRadius: 18,
+  padding: theme.spacing(0, 3),
+
+  transform: 'rotate(-2.16deg)',
+}));
+
+const DefaultButton = styled(StyledMUIButton)(({ theme }) => ({
+  color: theme.palette.brand.black,
 }));
 
 const RedMUIButton = styled(StyledMUIButton)(({ theme }) => ({
   border: `solid 2px ${theme.palette.brand.white}`,
 
   color: theme.palette.brand.white,
-
-  '&:before': {
-    backgroundColor: theme.palette.brand.red,
-  },
-
-  '&:hover': {
-    '&:before': {
-      backgroundColor: theme.palette.brand.red20,
-    },
-  },
-
-  '&:active': {
-    '&:before': {
-      backgroundColor: theme.palette.brand.red40,
-    },
-  },
 }));
 
 const OutlineMUIButton = styled(StyledMUIButton)(({ theme }) => ({
   border: `solid 2px ${theme.palette.brand.black}`,
 
   color: theme.palette.brand.black,
-
-  '&:before': {
-    display: 'none',
-  },
 
   '&:hover': {
     backgroundColor: theme.palette.brand.black,
@@ -85,10 +90,6 @@ const OutlineMUIButton = styled(StyledMUIButton)(({ theme }) => ({
   },
 }));
 
-const DefaultButton = styled(StyledMUIButton)(({ theme }) => ({
-  color: theme.palette.brand.black,
-}));
-
 type ButtonProps = {
   variant?: 'red' | 'default' | 'outline';
 };
@@ -96,22 +97,32 @@ export const Button: FC<PropsWithChildren<ButtonProps>> = ({
   variant = 'default',
   children,
 }) => {
+  let button = (
+    <DefaultButton className="button__body">
+      <Typography variant="button">{children}</Typography>
+    </DefaultButton>
+  );
+
   if (variant === 'red') {
-    return (
-      <RedMUIButton>
+    button = (
+      <RedMUIButton className="button__body">
         <Typography variant="button">{children}</Typography>
       </RedMUIButton>
     );
   } else if (variant === 'outline') {
-    return (
-      <OutlineMUIButton variant="outlined">
+    button = (
+      <OutlineMUIButton variant="outlined" className="button__body">
         <Typography variant="button">{children}</Typography>
       </OutlineMUIButton>
     );
   }
+
   return (
-    <DefaultButton>
-      <Typography variant="button">{children}</Typography>
-    </DefaultButton>
+    <Container>
+      {button}
+      {variant !== 'outline' && (
+        <Background className="button__background" variant={variant} />
+      )}
+    </Container>
   );
 };
