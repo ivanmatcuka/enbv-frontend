093b0217f660dfe5a04b89ff0a208bacd624b02e
Ivan Matcuka <ivanmatcuka@protonmail.com>
2023-11-27 22:39:03 +0100

    WIP: PersonCard component

---
+++ components/organisms/PersonCard/PersonCard.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { PersonCard as Component } from './PersonCard';
+
+const meta = {
+  title: 'Organisms/PersonCard',
+  component: Component,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Component>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const PersonCardBig: Story = {
+  args: {
+    size: 'l',
+    photoUrl:
+      'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200',
+    name: 'Габышев Александр',
+    subtitle: '18 сентября',
+  },
+};
+
+export const PersonCardSmall: Story = {
+  args: {
+    size: 'm',
+    photoUrl:
+      'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200',
+    name: 'Габышев Александр',
+    subtitle: '18 сентября',
+  },
+};
