7e5ec6463159e0817a348314b3fcfe4cbca7680b
Ivan Matcuka <ivanmatcuka@protonmail.com>
2023-12-18 21:44:31 +0100

    feat: card PZ

---
+++ components/organisms/CardPZ/CardPZ.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { CardPZ as Component } from './CardPZ';
+
+import { Button } from '../../atoms/Button/Button';
+
+const meta = {
+  title: 'Organisms/CardPZ',
+  component: Component,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Component>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const CardPZ: Story = {
+  args: {
+    name: 'Габышев Александр Александрович',
+    body: 'Согласно постановлению о возбуждении дела, он «с 6 марта по 22 мая, находясь в неустановленном месте, обратился лично в устной форме к группе людей в общественном месте, то есть публично, с призывами к осуществлению экстремистской деятельности».',
+    articles: ['ст. 280 УК РФ', 'ст. 280 УК РФ', 'ст. 280 УК РФ'],
+    primaryAction: <Button>Написать</Button>,
+    secondaryAction: <Button variant="outline">Помочь</Button>,
+    pictureUrl: '/card_pz_photo.png',
+  },
+};
---
+++ components/organisms/CardPZ/CardPZ.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import { Grid, styled } from '@mui/material';
+import Image from 'next/image';
+import { FC, ReactNode } from 'react';
+
+import { Article } from '../../atoms/Article/Article';
+import { Typography } from '../../typography/Typography/Typography';
+
+const Container = styled(Grid)({
+  position: 'relative',
+
+  paddingBottom: 64,
+  width: 392,
+  height: 447,
+  boxSizing: 'border-box',
+
+  background: 'url("/card_background.png") no-repeat',
+  backgroundSize: 'contain',
+});
+
+const PrimaryActionContainer = styled('div')(({ theme }) => ({
+  position: 'absolute',
+  left: theme.spacing(2),
+  bottom: theme.spacing(2),
+}));
+
+const SecondaryActionContainer = styled('div')(({ theme }) => ({
+  position: 'absolute',
+  right: theme.spacing(2),
+  bottom: theme.spacing(2),
+}));
+
+const StyledImage = styled(Image)({
+  position: 'absolute',
+  top: -40,
+  left: -6,
+});
+
+type CardPZProps = {
+  name: string;
+  body: string;
+  articles?: string[];
+  primaryAction?: ReactNode;
+  secondaryAction?: ReactNode;
+  pictureUrl?: string;
+};
+export const CardPZ: FC<CardPZProps> = ({
+  name,
+  body,
+  articles,
+  primaryAction,
+  secondaryAction,
+  pictureUrl,
+}) => {
+  return (
+    <Container container flexDirection="column" rowSpacing={2}>
+      <Grid item>
+        <Typography variant="h3" component="p" pl={17.5}>
+          {name}
+        </Typography>
+      </Grid>
+      <Grid pl={2} pr={2} item>
+        <Grid container spacing={0.5}>
+          {articles?.map((article, index) => (
+            <Grid item key={index}>
+              <Article label={article} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Typography variant="p3" component="p" pl={2}>
+          {body}
+        </Typography>
+      </Grid>
+      {pictureUrl && (
+        <StyledImage
+          alt="icon_letter"
+          width={135}
+          height={121}
+          src={pictureUrl}
+        />
+      )}
+      {primaryAction && (
+        <PrimaryActionContainer>{primaryAction}</PrimaryActionContainer>
+      )}
+      {secondaryAction && (
+        <SecondaryActionContainer>{secondaryAction}</SecondaryActionContainer>
+      )}
+    </Container>
+  );
+};
