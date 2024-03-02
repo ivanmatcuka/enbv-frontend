ce92787cb93abfc7ade31e1137313a9eeb546677
Ivan Matcuka <ivanmatcuka@protonmail.com>
2024-03-02 16:49:23 +0100

    fix: frame fixed

--- app/components/DrawingFrame/DrawingFrame.tsx
+++ app/components/DrawingFrame/DrawingFrame.tsx
@@ -1,20 +1,14 @@
 'use client';
 
 import { Grid, GridProps, styled } from '@mui/material';
-import Image from 'next/image';
 import { FC, useRef } from 'react';
 
 const Container = styled(Grid)({
   position: 'relative',
-});
-
-const FrameImage = styled(Image)({
-  position: 'absolute',
-  inset: 0,
-  zIndex: -1,
-
-  width: '100%',
-  height: '100%',
+  borderImageSource: `url("/images/frames/frame_default.svg")`,
+  borderImageSlice: 50,
+  borderImageWidth: 5,
+  borderStyle: 'solid',
 });
 
 export const DrawingFrame: FC<GridProps> = ({ children, ...rest }) => {
@@ -23,12 +17,6 @@ export const DrawingFrame: FC<GridProps> = ({ children, ...rest }) => {
   return (
     <Container {...rest} ref={containerRef}>
       {children}
-      <FrameImage
-        alt="frame"
-        width={300}
-        height={300}
-        src="/images/frames/frame_default.svg"
-      />
     </Container>
   );
 };
